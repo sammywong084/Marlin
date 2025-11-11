55d73b495b233c6c23c40dfaf2a15fe6f3503f36
sammywong084 <55323279+sammywong084@users.noreply.github.com>
2025-11-10 23:42:02 -0600

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -130,7 +130,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E3 Dual-Z 2.1.2.5 102025"
+#define CUSTOM_MACHINE_NAME "E3 Dual-Z 2.1.2.5 111025"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
